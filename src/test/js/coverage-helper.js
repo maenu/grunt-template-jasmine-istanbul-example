aad93770e057476cbfef68fe67da8a7d641e03ec
Manuel Leuenberger <manuel.leu@students.unibe.ch>
2013-02-12 00:00:06 +0100

    closure for helper

--- src/test/js/coverage-helper.js
+++ src/test/js/coverage-helper.js
@@ -1,5 +1,7 @@
-var reporter = new jasmine.Reporter();
-reporter.reportRunnerResults = function (runner) {
-	phantom.sendMessage('jasmine.coverage', __coverage__);
-};
-jasmine.getEnv().addReporter(reporter);
+(function () {
+	var reporter = new jasmine.Reporter();
+	reporter.reportRunnerResults = function (runner) {
+		phantom.sendMessage('jasmine.coverage', __coverage__);
+	};
+	jasmine.getEnv().addReporter(reporter);
+})();
